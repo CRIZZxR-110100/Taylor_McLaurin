109a82a08ebb82db8bb74d4b241ea7be196c459a
CRIZZ <cristobal.rafael.lara@gmail.com>
2025-01-29 17:40:37 -0600

    Versión inicial del Programa de Taylor Y MacLaurin

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 (que yo recuerde) Falta:
 
-- Las demas funciones fuera de sen(x).
+- Los demas polinomios de Taylor, fuera de sen(x).
+- Agregar las demas funcones orginales.
+- Que cuando se vuelva a presionar el boton de _Calcular_ se sobre escriba el gráfico anterior
 - Mejorar (si se puede) el cachado de errores, de que mensajes más especificos.
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import linspace as arr
 import math as m
 import tkinter as tk
 
@@ -77,6 +77,51 @@ def serie(x, grado, tipo):
       y.append(y_temp)
     return y
 
+def calcReal(x, tipo):
+  y = []
+
+  # Caso función e^x
+  if (tipo == 0):
+    for i in x:
+      y.append(m.exp(i)) 
+    return y
+
+  # Caso e^-x
+  if (tipo == 1):
+    for i in x:
+      y.append(m.exp(-i))
+    return y
+
+  # Caso sen(x)
+  if (tipo == 2):
+    for i in x:
+      y.append(m.sin(i))
+    return y
+
+  # Caso cos(x)
+  if (tipo == 3):
+    for i in x:
+      y.append(m.cos(i))
+    return y
+
+  # Caso senh(x)
+  if (tipo == 4):
+    for i in x:
+      y.append(m.sinh(i))
+    return y
+
+  # Caso cosh(x)
+  if (tipo == 5):
+    for i in x:
+      y.append(m.cosh(i))
+    return y
+
+  # Caso ln(1+x)
+  if (tipo == 6):
+    for i in x:
+      y.append(m.log(1+i))
+    return y
+
 def setGraph(a_ui, b_ui, n_ui, g1_ui, g2_ui, g3_ui, funcs_ui):
   a = float(0)
   b = float(0)
@@ -90,6 +135,10 @@ def setGraph(a_ui, b_ui, n_ui, g1_ui, g2_ui, g3_ui, funcs_ui):
   try:
     if funcs_ui.curselection():
       opc = funcs_ui.curselection()[0]
+    else:
+      tk.messagebox.showerror("Error", "No selecciono una función")
+      print("Error: Función no seleccionada")
+      return
 
     a = float(a_ui.get())
     b = float(b_ui.get())
@@ -101,24 +150,30 @@ def setGraph(a_ui, b_ui, n_ui, g1_ui, g2_ui, g3_ui, funcs_ui):
   except ValueError:
     tk.messagebox.showerror("Error", "Ingrese un número válido")
     print("Error: Ingrese un número válido")
+    return
 
   if (opc < 0 or opc > 6):
     tk.messagebox.showerror("Error", "Seleccione una función")
     print("Error: Seleccione una función")
     return
   
-  x = np.linspace(a , b, n)
+  x = arr(a , b, n)
   y1 = serie(x, g1, opc)
   y2 = serie(x, g2, opc)
   y3 = serie(x, g3, opc)
+
+  org = calcReal(x, opc)
   
   fig = Figure(figsize=(5, 4), dpi=100)
   ax = fig.add_subplot(111)
 
-  ax.plot(x, y1)
-  ax.plot(x, y2)
-  ax.plot(x, y3)
-  ax.plot(x, org)
+  ax.plot(x, org, color="orange", label="f(x)")
+
+  ax.plot(x, y1, color="green", label=f"T{g1}(x)")
+  ax.plot(x, y2, color="blue", label=f"T{g2}(x)")
+  ax.plot(x, y3, color="red", label=f"T{g3}(x)")
+
+  ax.legend(loc="upper right", fontsize="small")
 
   ax.set_title("Pólinomios de Taylor y McLaurin")
   ax.set_xlabel("x")
@@ -149,17 +204,6 @@ g3_ui = tk.StringVar(value="")
 opciones = tk.Frame(root, width=300, height=450, borderwidth=2, relief="sunken")
 opciones.pack(padx=20, pady=10, side=tk.RIGHT)
 
-# Lista de las funciones disponibles
-etiqueta = tk.Label(opciones, text="Funciones")
-etiqueta.pack(padx=10)
-opc = tk.StringVar(value="")
-funcs = tk.Listbox(opciones, selectmode=tk.SINGLE, width=20, height=5, listvariable=opc)
-funcs.pack(padx=10, pady=2)
-
-# Llenado de la lista de funciones
-for item in ["Exponente", "Exponente ^-", "Seno", "Coseno", "Seno Hiperbolico", "Coseno Hiperbolico", "Logaritmo natural"]:
-  funcs.insert(tk.END, item, )
-
 # Campos de texto para introducir los grados
 etiqueta2 = tk.Label(opciones, text="Grados")
 etiqueta2.pack(padx=10)
@@ -183,6 +227,17 @@ etiqueta5 = tk.Label(opciones, text="Cantidad de puntos")
 etiqueta5.pack(padx=10)
 pts = tk.Entry(opciones, width=20, textvariable=n_ui)
 pts.pack(padx=10, pady=2)
+
+# Lista de las funciones disponibles
+etiqueta = tk.Label(opciones, text="Funciones")
+etiqueta.pack(padx=10)
+opc = tk.StringVar(value="")
+funcs = tk.Listbox(opciones, selectmode=tk.SINGLE, width=20, height=5, listvariable=opc)
+funcs.pack(padx=10, pady=2)
+
+# Llenado de la lista de funciones
+for item in ["Exponente", "Exponente ^-", "Seno", "Coseno", "Seno Hiperbolico", "Coseno Hiperbolico", "Logaritmo natural"]:
+  funcs.insert(tk.END, item, )
 
 # Boton que realiza el cálculo
 calc = tk.Button(opciones, text="Calcular", width=15,command=lambda:setGraph(a_ui, b_ui, n_ui, g1_ui, g2_ui, g3_ui, funcs) )
